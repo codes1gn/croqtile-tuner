ae6c5c9323c76e80ee3d6d8c55ba6cc956b8e2e0
heng shi <heng.shi@sjtu.edu.cn>
2026-04-19 03:57:28 +0800

    tune(croqtile): matmul f16fp32 2048x16384x2048 - iter018 264.555 TFLOPS

---
+++ tuning/sm90_NVIDIA_H800_PCIe/croqtile/cmd/matmul_f16fp32_2048x16384x2048/gpt-5/run_iter018.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+BIN="tuning/sm90_NVIDIA_H800_PCIe/croqtile/bin/matmul_f16fp32_2048x16384x2048/gpt-5/iter018_wait1"
+LOG="tuning/sm90_NVIDIA_H800_PCIe/croqtile/perf/matmul_f16fp32_2048x16384x2048/gpt-5/timing_iter018.txt"
+"$BIN" "$@" 2>&1 | tee "$LOG"
